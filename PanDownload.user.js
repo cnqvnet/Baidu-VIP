07959043ccf970dd613875bc1c50310f57278563
itxh <892457803@qq.com>
2024-12-05 09:36:03 +0800

    2024年12月5日：更新百度网盘加速脚本

--- PanDownload.user.js
+++ PanDownload.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name              文武Download-高速下载直链的百度云盘SVIP助手
 // @namespace         https://github.com/dongyubin/Baidu-VIP
-// @version           1.3
-// @description       提取百度网盘单文件高速直链、便捷使用的脚本助手，支持 Gopeed 多线程下载工具。向广大网友免费交流学习使用，探索使用脚本的乐趣！
+// @version           1.4
+// @description       百度网盘单文件SVIP解析高速直链、便捷使用的脚本助手，100%可用，支持 Gopeed、IDM、NDM 等多线程下载工具。向广大网友免费交流学习使用，探索使用脚本的乐趣！
 // @author            dongyubin
 // @homepage          https://fk.wwkejishe.top/buy/23
 // @license           MIT
