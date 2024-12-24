8c2aa7cb7e08b8c268ce20ddd9fae3a4ccc61140
itxh <892457803@qq.com>
2024-12-24 16:08:01 +0800

    2024年12月24日:新增文武直链助手

---
+++ PanDownloadLocal.md
@@ -0,0 +1,48 @@
+# 文武直链助手
+
+🛒网盘极速下载VIP账号：[立即前往](https://fk.wwkejishe.top/buy/23)
+
+Github By [@dongyubin](https://github.com/dongyubin/Baidu-VIP)
+
+## 概述 / Overview
+
+只支持在 [文武Download网页版](https://pandown.mlover.site/) 使用，不支持在百度网盘网页端使用。
+
+## 特别提示 / hot tip
+
+网页需要下载网盘获取`解析密码`才能解锁全部功能，请看清楚再用！！！
+
+[获取临时 SVIP 账号](https://fk.wwkejishe.top/buy/23)、[获取解析验证码](https://www.wangdu.site/software/tools/948.html)
+
+## 渲染界面 / Rendering interface
+
+![下载速度展示](https://cdn.wwkejishe.top/wp-cdn-02/2024/202411171326300.webp)
+
+## 兼容的桌面端浏览器 / Supported browsers
+
+### [Google Chrome](https://www.google.cn/intl/zh-CN/chrome/) | [Firefox](https://www.firefox.com.cn/) | [Microsoft Edge](https://www.microsoft.com/zh-cn/edge)
+
+建议测试本脚本安装上述最新的浏览器，因为第三方 Chome 或精简的浏览器也会出现脚本不能正常使用的情况。
+
+It is recommended to test this script and install the latest browser mentioned above, as third-party chome or simplified browsers may also encounter situations where the script cannot be used properly.
+
+## 声明 / Statement
+
+1、脚本未最小化或混淆 Script not minimized or confused
+
+2、未引用不符合规则的外部资源 Unreferenced external resources that do not comply with rules
+
+3、脚本符合Greasyfork **会员内容** The script conforms to Greasyfork **membership** content
+
+- 这个脚本会在你访问的网站插入**广告**。
+  This script will insert **advertisements** on the website you visit
+
+- 你只有在 参与某些事后 才能使用脚本的全部功能，如**加入某群组、订阅某频道或在某页点赞等**。
+  You can only use all the functions of the script after participating in certain activities, such as joining a group, subscribing to a channel, or liking a page.
+
+4、脚本含有追踪行为，使用脚本责代表你**同意该条款**。
+
+The script contains tracking behavior, and using the script represents your **agreement to this clause.**
+
+举报前三思,瞎举报会被**Ban**
+According to the Greasyfork rules, reporting from malicious companies will result in
---
+++ PanDownloadLocal.user.js
@@ -0,0 +1,114 @@
+// ==UserScript==
+// @name         文武直链助手-百度网盘在线解析网页直链获取助手
+// @namespace    https://github.com/dongyubin/Baidu-VIP
+// @version      0.1
+// @description  不限制速度的百度网盘SVIP解析直链网页获取助手，支持 Gopeed（一键解析）、IDM、NDM 等多线程极速下载工具
+// @author       DongYubin
+// @homepage     https://fk.wwkejishe.top/buy/23
+// @supportURL   https://fk.wwkejishe.top/buy/23
+// @license      MIT
+// @match        https://pandown.mlover.site/*
+// @icon         https://fk.wwkejishe.top/uploads/images/6e798005b00ce678782af4e6931f4374.png
+// @resource     layuiCSS https://cdnjs.cloudflare.com/ajax/libs/layui/2.9.20/css/layui.min.css
+// @require      https://cdnjs.cloudflare.com/ajax/libs/layui/2.9.20/layui.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js
+// @grant        GM_cookie
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @grant        GM_setClipboard
+// ==/UserScript==
+const layuiCss = GM_getResourceText('layuiCSS');
+GM_addStyle(layuiCss);
+const wwConfig = {
+  ua: 'netdisk;1.0.1',
+};
+function addXMLRequestCallback(callback) {
+  var oldSend, i;
+  if (XMLHttpRequest.callbacks) {
+    // we've already overridden send() so just add the callback
+    XMLHttpRequest.callbacks.push(callback);
+  } else {
+    // create a callback queue
+    XMLHttpRequest.callbacks = [callback];
+    // store the native send()
+    oldSend = XMLHttpRequest.prototype.send;
+    // override the native send()
+    XMLHttpRequest.prototype.send = function () {
+      // process the callback queue
+      for (i = 0; i < XMLHttpRequest.callbacks.length; i++) {
+        XMLHttpRequest.callbacks[i](this);
+      }
+      // call the native send()
+      oldSend.apply(this, arguments);
+    }
+  }
+}
+
+// e.g.
+addXMLRequestCallback(function (xhr) {
+  xhr.addEventListener("load", function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      const url = xhr.responseURL;
+      // console.log('拦截返回：', xhr);
+      if (url.includes('https://aifenxiang.net.cn:8081/wp/fast/pc/dlink')) {
+        try {
+          const responseData = JSON.parse(xhr.responseText);
+          const downloadUrl = responseData.data.data.urls[0].url;
+          layer.confirm(`未下载成功，先设置IDM/NDM User-Agent:<code>` + wwConfig.ua + `</code>，再复制直链下载！`,
+            {
+              btn: ['已下载，关闭弹窗', '未下载，复制直链'],
+              closeBtn: 0,
+            }, function (index) {
+              layer.close(index);
+            }, function () {
+              GM_setClipboard(downloadUrl, "text");
+              layer.msg(`直链复制成功！`);
+              layer.close(index);
+            });
+          // alert('下载链接: ' + downloadUrl);
+        } catch (e) {
+          console.error('解析响应时出错: ', e);
+        }
+      } else if (url.includes('https://aifenxiang.net.cn:8081/wp/dlink')) {
+        try {
+          const responseData = JSON.parse(xhr.responseText);
+          const downloadUrl = responseData.data.data[0].url;
+          layer.confirm(`未下载成功，先设置IDM/NDM User-Agent:<code>` + wwConfig.ua + `</code>，再复制直链下载！`,
+            {
+              btn: ['已下载，关闭弹窗', '未下载，复制直链'],
+              closeBtn: 0,
+            }, function (index) {
+              layer.close(index);
+            }, function () {
+              GM_setClipboard(downloadUrl, "text");
+              layer.msg(`直链复制成功！`);
+              layer.close(index);
+            });
+          // alert('下载链接: ' + downloadUrl);
+        } catch (e) {
+          console.error('解析响应时出错: ', e);
+        }
+      }
+    }
+  });
+});
+
+// Intercept fetch requests
+// (function () {
+//   const originalFetch = window.fetch;
+//   window.fetch = function () {
+//     return originalFetch.apply(this, arguments).then(async response => {
+//       const url = response.url;
+//       if (url.includes('http://127.0.0.1:9999/api/v1/tasks')) {
+//         try {
+//           const responseClone = response.clone();
+//           const responseData = await responseClone.json();
+//           console.log('拦截请求：', responseData);
+//         } catch (e) {
+//           console.error('解析响应时出错: ', e);
+//         }
+//       }
+//       return response;
+//     });
+//   };
+// })();
