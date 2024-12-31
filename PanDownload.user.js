d6ed5faf629b4abb6cba88ce7ef7786cbd9ee3db
itxh <892457803@qq.com>
2024-12-31 21:31:01 +0800

    2024年12月31日:更新百度网盘不限速脚本api地址

--- PanDownload.user.js
+++ PanDownload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              百度网盘SVIP高速解析直链的不限速下载助手-文武PanDownload
 // @namespace         https://github.com/dongyubin/Baidu-VIP
-// @version           4.8
+// @version           4.9
 // @description       不限制速度的百度网盘SVIP解析高速直链的脚本助手，无视黑号，100%可用，下载速度最快可达10M+/s，支持 Gopeed（一键解析）、IDM、NDM 等多线程极速下载工具，支持 Microsoft Edge、Google Chrome、Firefox 等浏览器。
 // @author            dongyubin
 // @homepage          https://fk.wwkejishe.top/buy/23
@@ -525,7 +525,7 @@
               });
               if (res.code == 200) {
                 wwConfig.code = code;
-                if (res.data > 100 || (res.data.data == 100 & res.data.vip == 0)) {
+                if (res.data > 100 || (res.data.data == 100 & res.data.vip == 1)) {
                   let download_url = '';
                   switch (type) {
                     case 1:
